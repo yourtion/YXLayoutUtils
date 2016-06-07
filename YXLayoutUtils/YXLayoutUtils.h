2dd80741ecf8b3a9db554223a19037797cec9a3f
YourtionGuo <yourtion@gmail.com>
2016-06-07 15:12:53 +0800

    Initial Commit

---
+++ YXLayoutUtils/YXLayoutUtils.h
@@ -0,0 +1,19 @@
+//
+//  YXLayoutUtils.h
+//  YXLayoutUtils
+//
+//  Created by YourtionGuo on 6/7/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for YXLayoutUtils.
+FOUNDATION_EXPORT double YXLayoutUtilsVersionNumber;
+
+//! Project version string for YXLayoutUtils.
+FOUNDATION_EXPORT const unsigned char YXLayoutUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YXLayoutUtils/PublicHeader.h>
+
+
